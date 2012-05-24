0d003ee26d89b3e38fc0a5f8a77f6edbeb490a58
Kees Huiberts <itissohardtothinkofagoodemail@gmail.com>
2012-05-24 09:20:42 +0200

    Changed indentation and such

--- src/org/beide/bomber/BomberActivity.java
+++ src/org/beide/bomber/BomberActivity.java
@@ -3,13 +3,10 @@ package org.beide.bomber;
 import android.app.Activity;
 import android.os.Bundle;
 
-public class BomberActivity extends Activity
-{
-    /** Called when the activity is first created. */
-    @Override
-    public void onCreate(Bundle savedInstanceState)
-    {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.main);
-    }
+public class BomberActivity extends Activity {
+	
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.main);
+	}
 }
