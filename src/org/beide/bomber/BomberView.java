16dbb43967ca0c0ec5a489a219ef5a62f0c770a8
Kees Huiberts <itissohardtothinkofagoodemail@gmail.com>
2012-06-02 19:00:35 +0200

    typo

--- src/org/beide/bomber/BomberView.java
+++ src/org/beide/bomber/BomberView.java
@@ -27,7 +27,7 @@ public class BomberView extends SurfaceView implements SurfaceHolder.Callback {
 		
 		int[][] levels = {
 			{0,1,2,3,4,5,4,3,2,1},
-			{5,5,5,5,5,5,5,4,5,5}
+			{5,5,5,5,5,5,5,4,5,5},
 			{9,8,7,6,5,4,3,2,1,0}
 		};
 		
